731bbf26c2868a18078eaaa1983605bf0f1e86e1
Amil P A <amilpa2020@gmail.com>
2023-03-23 21:21:56 +0530

    minor updates

---
+++ README.md
@@ -0,0 +1,9 @@
+# Simple react chat app with firebase backend
+
+This is a simple react chat app made using firebase as backup with firestore database.
+<br>
+Users can log on to the app onto a single group chat section where anyone can send text messages.
+<br>
+There are functions that check for profanity to ban users who use unwanted vocabulary;).
+<br>
+A NoSQL database is used(firestore) for storing messages.
--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,23 @@
-const functions = require("firebase-functions");
-const Filter = require("bad-words");
-
-const admin = require("firebase-admin");
-admin.initializeApp();
-
-const db = admin.firestore();
-
-exports.detectEvilUsers = functions.firestore
-    .document("my-collection/{docId}")
-    .onCreate( async (doc, ctx) => {
-      const filter = new Filter();
-      const {text, uid} = doc.data();
-
-      if (filter.isProfane(text)) {
-        await doc.ref.update({text: "I got banned for life"});
-
-        await db.collection("banned").doc(uid).set({});
-      }
-    });
+// const functions = require("firebase-functions");
+// const Filter = require("bad-words");
+//
+// const admin = require("firebase-admin");
+// admin.initializeApp();
+//
+// const db = admin.firestore();
+//
+// exports.detectEvilUsers = functions.firestore
+//     .document("my-collection/{docId}")
+//     .onCreate( async (doc, ctx) => {
+//       const filter = new Filter();
+//       const {text, uid} = doc.data();
+//
+//       if (filter.isProfane(text)) {
+//         await doc.ref.update({text: "I got banned for life"});
+//
+//         await db.collection("banned").doc(uid).set({});
+//       }
+//     });
 
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
